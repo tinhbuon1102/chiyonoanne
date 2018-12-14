8b34da5921143680f5ca37d2b82d7acc92a3217b
tinhbuon1102 <quocthang.2001@gmail.com>
2018-12-14 18:34:51 +0900

    Deploy files From Test Environment

--- wp-content/themes/zoa-child/functions.php
+++ wp-content/themes/zoa-child/functions.php
@@ -40,7 +40,7 @@ function zoa_enqueue_parent_theme_style() {
 					 array('zoa-theme-style'),
 					 date('YmdHis',filemtime( get_stylesheet_directory(). '/style.css'))
 					);*/
-	wp_enqueue_style( 'ec-style', get_stylesheet_directory_uri() . '/css/woo.css?201812141138', array('zoa-child-style'));
+	wp_enqueue_style( 'ec-style', get_stylesheet_directory_uri() . '/css/woo.css?201812141834', array('zoa-child-style'));
 	wp_enqueue_style( 'loading-style', get_stylesheet_directory_uri() . '/css/loading.css', array('ec-style'));
 }
 
