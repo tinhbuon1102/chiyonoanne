462645a9c18c7dbd9bad65e103f63d45b12f990f
tinhbuon1102 <quocthang.2001@gmail.com>
2018-12-19 06:49:45 +0900

    Deploy files From Test Environment

--- wp-content/plugins/everest-admin-theme/inc/backend/main_page.php
+++
@@ -1,5 +0,0 @@
-<?php
-defined( 'ABSPATH' ) or die( "No script kiddies please!" );
-include('common/header.php');
-include('plugin-settings.php');
-include('common/footer.php');
--- wp-content/plugins/everest-admin-theme/js/customize.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function($) {
-	//$('body').addClass('eat-body-class-wrap eat-dashboard-temp-4')
-});
