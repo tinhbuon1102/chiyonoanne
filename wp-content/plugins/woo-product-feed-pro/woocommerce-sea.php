2372fd46458dba8ec74785ae8c32dc2c7b47e8db
thangtqvn <thangtqvn@MacBook-Pro-cua-thangtqvn.local>
2019-01-16 12:14:06 +0700

    Upload files

--- wp-content/plugins/woo-product-feed-pro/woocommerce-sea.php
+++ wp-content/plugins/woo-product-feed-pro/woocommerce-sea.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Product Feed PRO for WooCommerce
- * Version:     4.2.3
+ * Version:     114.2.3
  * Plugin URI:  https://www.adtribes.io/support/?utm_source=wpadmin&utm_medium=plugin&utm_campaign=woosea_product_feed_pro
  * Description: Configure and maintain your WooCommerce product feeds for Google Shopping, Facebook, Remarketing, Bing, Yandex, Comparison shopping websites and over a 100 channels more.
  * Author:      AdTribes.io
