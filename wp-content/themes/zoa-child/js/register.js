8b07d71d7c573f9550c0dc05aff9514ee2297d54
thangtqvn <thangtqvn@MacBook-Pro-cua-thangtqvn.local>
2019-01-29 22:30:08 +0700

    Upload files

--- wp-content/themes/zoa-child/js/register.js
+++ wp-content/themes/zoa-child/js/register.js
@@ -9,9 +9,9 @@ jQuery(function($){
 	
 	function populateBirthField()
 	{
-		$('#birth_year option:q(0)').text('-----');
-		$('#birth_month option:q(0)').text('-----');
-		$('#birth_day option:q(0)').text('-----');
+		$('#birth_year option:eq(0)').text('-----');
+		$('#birth_month option:eq(0)').text('-----');
+		$('#birth_day option:eq(0)').text('-----');
 		
 		for (var i=1930; i<=2017; i++){
 			$('#birth_year').append('<option value="'+ i +'">'+ i +'</option>');
